a8b8ecd4cc35963a1aa5903ea04560fed3053125
pronapro <nakirandaproscovia@gmail.com>
2021-01-02 21:15:28 +0300

    more

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# alchemy-with-friends-cards
-I am using the Microsoft Alchemy with friends cards to learn new/improve on computer science,Artificial intelligence,machine learning and tech concepts,  This is a function to help randomly select a card I should use for learning.I will be getting the recommendation daily which is one card.
-## The cards i have 
+# Alchemy-with-friends-cards
+I am using the Microsoft Alchemy with friends cards to learn new/improve on computer science,Artificial Intelligence,machine learning and tech concepts,  This is a function to help randomly select a card I should use for learning.I will be getting the recommendation daily which is one card.
+## The cards I have 
 ## How the cards are played
-## Why i am playing it differntly
+## Why I am playing it differently
---
+++ app.py
@@ -0,0 +1,10 @@
+import random
+def random_select():
+	white_cards =["The prosecutor fallacy","catastrophic forgetting","predatory bootcamps","Adversarial examples","Differential privacy","50+ billion parameters", "skipping on a multiple testing correction","Deep learning celebrities","Random noise","Optimizing the most convenient objective",
+	"The Neurips random ballot","plush giraffe perturbation","pretraining","1000 GPUs", "information bottleneck","A theorem","Relational bias","Dataset selection","Learning more math", "Mathiness","Gradient explosion"
+	"Non-parametric","Huggingface","Causality","Deep fakes","Checking arXiv obsessively","Moving sliders at http://distill.pub/","Sunday night deadlines","learning on the edge","Sparsity","OpenReview"]
+	black_cards =["Deep ___", "_______is the largest model trained yet","your baseline experiment should include____","All i want in life is ___",
+	"_____ is the problem, ____is the solution","______ is just an attention mechanism","Deep Generative Models are all about____","Anyone else having problems with ____today, or is it just me","Training ______ is easy; just use _______","Artificial General Intelligence will be solved by ______","______ achieves superhuman performance", "My start-up applies ___ to solve _________"]
+	print(len(white_cards))
+	print(len(black_cards))
+random_select()
